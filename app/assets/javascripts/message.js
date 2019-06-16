157a7ff95573be2071e26c742ca4fc45d8353b00
Tanaka Koyo <tanakakoyo1999@icloud.com>
2019-06-16 20:53:33 +0900

    Summary(required)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,6 @@
 $(function(){
+  var chat_main = $('.chat_main_center');
+
   $form = $($.rails.formSubmitSelector)
   function buildHTML(message){
     var img = ""
@@ -39,9 +41,9 @@ $(function(){
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.chat_main_center').append(html)
+      chat_main.append(html)
       $('.form__message').val('')
-      $('.chat_main_center').animate({
+      chat_main.animate({
         scrollTop: 1000
     }, 1050);
       $.rails.enableFormElements($form)
@@ -51,4 +53,81 @@ $(function(){
       alert('error');
     })
   })
+
+
+  var buildMessageHTML = function(message) {
+    if (message.content && message.image.url) {
+      var html = '<div class="chat_main_center-message" data-id=' + message.id + ' data-class='+message.group+'>' +
+                  '<div class="chat_main_center-message-top">' +
+                    '<div class="chat_main_center-message-top-user">' +
+                      message.user_name +
+                    '</div>' +
+                    '<div class="chat_main_center-message-top-date">' +
+                      message.created_at +
+                    '</div>' +
+                  '</div>' +
+                  '<div class="chat_main_center-message-detail ">' +
+                    '<p class="lower-message__content">' +
+                      message.content +
+                    '</p>' +
+                    '<img src="' + message.image.url + '" class="lower-message__image" >' +
+                  '</div>' +
+                '</div>'
+    } else if (message.content) {
+      var html = '<div class="chat_main_center-message" data-id=' + message.id + ' data-class='+message.group+'>' +
+                  '<div class="chat_main_center-message-top">' +
+                    '<div class="chat_main_center-message-top-user">' +
+                      message.user_name +
+                    '</div>' +
+                    '<div class="chat_main_center-message-top-date">' +
+                      message.created_at +
+                    '</div>' +
+                  '</div>' +
+                  '<div class="chat_main_center-message-detail ">' +
+                    '<p class="lower-message__content">' +
+                      message.content +
+                    '</p>' +
+                  '</div>' +
+                '</div>'
+    } else if (message.image.url) {
+      var html = '<div class="chat_main_center-message" data-id=' + message.id + ' data-class='+message.group+'>' +
+                  '<div class="chat_main_center-message-top">' +
+                    '<div class="chat_main_center-message-top-user">' +
+                      message.user_name +
+                    '</div>' +
+                    '<div class="chat_main_center-message-top-date">' +
+                      message.created_at +
+                    '</div>' +
+                  '</div>' +
+                  '<div class="chat_main_center-message-detail ">' +
+                    '<img src="' + message.image.url + '" class="lower-message__image" >' +
+                  '</div>' +
+                '</div>'
+    };
+    return html;
+  };
+  var reloadMessages = function() {
+    last_message_id = $('.chat_main_center-message:last').data('id');
+    last_group_id = $('.chat_main_center-message:last').data('class');
+    $.ajax({
+      url: "/groups/last_group_id/api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(data) {
+      $.each(data, function(i, message) {
+        var html = buildMessageHTML(message);
+        chat_main.append(html);
+      });
+      chat_main.animate({
+        scrollTop: 1000
+      }, 1050);
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+
+  setInterval(reloadMessages, 5000);
 });
